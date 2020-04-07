483f79b3c83e467ed18ea5b6556678d410772a58
Satheesh C <satheesancchandran@gmail.com>
2020-04-07 17:28:43 +0530

    Reversed for loop

--- atm.c
+++ atm.c
@@ -6,10 +6,10 @@ unsigned int get_money(unsigned short int amount)
   unsigned int notes = 0;
   if (amount > 31999) return notes;
   unsigned short int left_amound = amount;
-  int denominations[8] = {2000, 500, 100, 50, 20, 10, 5, 1};
-  for (int i = 0; i < 8; i++)
+  int denominations[8] = {1, 5, 10, 20, 50, 100, 500, 2000};
+  for (int i = 7; i >=0 ; i--)
   {
-    notes += pow(16, (7 - i)) * (left_amound / denominations[i]);
+    notes += pow(16, i) * (left_amound / denominations[i]);
     left_amound %= denominations[i];
   }
   return notes;
