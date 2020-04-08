9b29cc5755193749877b76aefb4bb18053c09944
Satheesh C <satheesancchandran@gmail.com>
2020-04-08 12:28:52 +0530

    Changed addition into bitwise or operation

--- atm.c
+++ atm.c
@@ -6,11 +6,12 @@ unsigned int get_money(unsigned short int amount)
   unsigned int notes = 0;
   if (amount > 31999) return notes;
   unsigned short int left_amound = amount;
-  int denominations[8] = {1, 5, 10, 20, 50, 100, 500, 2000};
-  for (int i = 7; i >=0 ; i--)
+  int denominations[8] = {1, 5, 10, 20, 50, 100, 500, 2000}, power;
+  for (int index = 7; index >=0 ; index--)
   {
-    notes += pow(16, i) * (left_amound / denominations[i]);
-    left_amound %= denominations[i];
+    power = pow(16, index);
+    notes |= power * (left_amound / denominations[index]);
+    left_amound %= denominations[index];
   }
   return notes;
 }
