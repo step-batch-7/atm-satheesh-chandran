3bfa6910eaca632ba3af790b959e7202be7b9828
Satheesh C <satheesancchandran@gmail.com>
2020-04-08 20:58:41 +0530

    Changed the power function into bitwise shift operation

--- atm.c
+++ atm.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <math.h>
 
 unsigned int get_money(unsigned short int amount)
 {
   unsigned int notes = 0;
   if (amount > 31999) return notes;
   unsigned short int left_amound = amount;
-  int denominations[8] = {1, 5, 10, 20, 50, 100, 500, 2000}, power;
+  int denominations[8] = {1, 5, 10, 20, 50, 100, 500, 2000};
   for (int index = 7; index >=0 ; index--)
   {
-    power = pow(16, index);
-    notes |= power * (left_amound / denominations[index]);
+    notes |= (1 << index * 4) * (left_amound / denominations[index]);
     left_amound %= denominations[index];
   }
   return notes;
